a0885ed89b0f49d4f86e7675420d45ac1a1b0c23
Nagi <nagiitoshi@gmail.com>
2025-03-09 13:02:57 -0300

    initial commit

---
+++ src/test/java/cryptography/challenge/cryptography_challenge/CryptographyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package cryptography.challenge.cryptography_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptographyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
